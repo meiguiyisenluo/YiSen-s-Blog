b65c20aec5466450ab12c960335a0b7d1ca16f83
lys <1402175410@qq.com>
2025-03-19 16:46:34 +0800

    fix

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -16,7 +16,7 @@ const projectsData: Project[] = [
   {
     title: 'nginx autoindx pretty ui',
     description: `nginx-pretty-autoindex, using VITE, Vue@3, and Vue Router@4, Windows File System - Large Image Viewer`,
-    imgSrc: '/static/images/2025-03-14-125420.png',
+    imgSrc: '/static/images/2025-03-19-163015.png',
     href: '/blog/02',
   },
   {
@@ -24,14 +24,22 @@ const projectsData: Project[] = [
     description: `VTRON 提供的大屏幕拼接显控产品及其解决方案，可根据不同客户的
 应用场景，通过各种屏幕（端），利用网络和各种应用（企业云），将
 信息、交互、协作整合在一起，满足用户信息智能化显示与控制的需求。 `,
-    imgSrc: '/static/images/2022021811174865.jpg',
+    imgSrc: '/static/images/2025-03-19-163016.png',
     href: 'https://www.vtron.com.cn/product_def_list_775.html',
   },
   {
     title: 'LEG处理器',
     description: `LEG是一种新的图灵完备的计算机架构，你可以利用它来解决一切算法问题。`,
-    imgSrc: '/static/images/blog_03/02.png',
+    imgSrc: '/static/images/2025-03-19-163014.png',
     href: '/blog/03',
+  },
+  {
+    title: 'OmgTV',
+    description: `OmgTV 是一款为全球用户打造的随机视频聊天平台，旨在打破边界，为人们带来意想不到的
+社交体验。在这里，每一次匹配都可能让你惊呼“OMG！”，从陌生人的日常故事到文化的奇
+妙碰撞，每一次相遇都是独一无二的。为用户提供了一个快速、安全且富有趣味的互动空间。`,
+    imgSrc: '/static/images/2025-03-19-163017.png',
+    href: 'https://luoyisen.com/client/#/omgTv',
   },
 ]
 
